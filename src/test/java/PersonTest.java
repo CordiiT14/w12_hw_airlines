592bc3dee7f351c49f3c43702bf6acfd7a0912e8
CordiiT14 <cordelia.toennies@live.com>
2022-05-15 12:36:07 +0100

    Initial commit

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PersonTest {
+
+    private Person person;
+
+    @Before
+    public void before(){
+        person = new Person("Joey");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Joey", person.getName());
+    }
+
+}
